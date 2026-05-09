09766c063cb0948f36777bc150e39e015695f3b1
PARKDEC <yj060618@bu.ac.kr>
2026-05-09 22:40:57 +0900

    onBack 완전히 삭제

--- src/components/CreateTeamSpace/Step2.tsx
+++ src/components/CreateTeamSpace/Step2.tsx
@@ -3,7 +3,6 @@ import { type TeamSpaceForm } from './types';
 interface Props {
   form: TeamSpaceForm;
   onChange: (patch: Partial<TeamSpaceForm>) => void;
-  //onBack: () => void;
   onSubmit: () => void;
 }
 
