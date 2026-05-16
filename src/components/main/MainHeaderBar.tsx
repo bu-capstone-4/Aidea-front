fe1241197a0fa32c28be452b03ba3441e91fa845
PARKDEC <yj060618@bu.ac.kr>
2026-05-16 20:52:23 +0900

    fix: OnlineMemerStack import 경로수정

--- src/components/main/MainHeaderBar.tsx
+++ src/components/main/MainHeaderBar.tsx
@@ -1,6 +1,6 @@
 import { useParams } from 'react-router';
 import Button from '@/components/ui/Button';
-import OnlineMemberStack from './OnlineMemberStack';
+import OnlineMemberStack from '@/components/ui/OnlineMemberStack';
 import { useTeamspaceStore } from '@/store/teamspaceStore';
 import { useTeamspaceDetail } from '@/hooks/useTeamspaceDetail';
 import { useDocument } from '@/hooks/useDocument';
