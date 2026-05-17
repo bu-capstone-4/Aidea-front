28d0ff4ce10c53cfe7a8c9be0fb8aec257f82557
kang-min-seok <min1042004444@gmail.com>
2026-05-17 15:51:25 +0900

    chore: husky 속도 개선
    - prettier가 파일 당 2번 실행 되던 문제 개선
    - ESLint 캐시 적용

--- eslint.config.js
+++ eslint.config.js
@@ -3,7 +3,6 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
-import prettier from 'eslint-plugin-prettier'
 import prettierConfig from 'eslint-config-prettier'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
@@ -18,12 +17,6 @@ export default defineConfig([
       reactRefresh.configs.vite,
       prettierConfig,
     ],
-    plugins: {
-      prettier,
-    },
-    rules: {
-      'prettier/prettier': 'error',
-    },
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
